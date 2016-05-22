1ab7ecbe32ec16e7f54c7d5dd5155d76990a918b
Gail <gail@gilamonster.za.net>
2016-05-22 23:52:08 +0200

    Spread out to be more readable, paste replaced with str_c, and more comments added

--- code_off-10/code_off-10-readable.R
+++ code_off-10/code_off-10-readable.R
@@ -16,22 +16,25 @@ MessageLetters <- str_split(Message, "") # split the message into individual cha
 MessageEncoded <- list(1:length(MessageLetters)) # Placeholder for the encoded letters
 
 #Get lists of dashes and dots, starting at 6 characters long and going down to 1
-ListOfDots <- lapply(lapply(c(MaxCharacterLength:1), function(x) rep.int(".", times = x)), function(x) str_join(x, collapse=''))
-ListOfDashes <- lapply(lapply(c(MaxCharacterLength:1), function(x) rep.int("-", times = x)), function(x) str_join(x, collapse=''))
+ListOfDots <- lapply(lapply(c(MaxCharacterLength:1), function(x) rep.int(".", times = x)), function(x) str_c(x, collapse=''))
+ListOfDashes <- lapply(lapply(c(MaxCharacterLength:1), function(x) rep.int("-", times = x)), function(x) str_c(x, collapse=''))
 # and calculate the correct encoding for each
 EncodingDot <- as.character(nchar(ListOfDots))
 EncodingDash <- sapply(nchar(ListOfDashes)+64,function(x) rawToChar(as.raw(x)))
 
 for (i in 1:MaxCharacterLength) { #Encode the original Morsecode, not the message. Should be faster than looping over a, potentially, long message
-  MorseCode$Encoding <- gsub(ListOfDots,EncodingDot,MorseCode$Encoding,fixed=TRUE) # replace the dots with their encoding
-  MorseCode$Encoding <- gsub(ListOfDashes,EncodingDash,MorseCode$Encoding, fixed=TRUE) # replace dashes with their encoding
+  MorseCode$Encoding <- gsub(ListOfDots[[i]],EncodingDot[[i]],MorseCode$Encoding,fixed=TRUE) # replace the dots with their encoding
+  MorseCode$Encoding <- gsub(ListOfDashes[[i]],EncodingDash[[i]],MorseCode$Encoding, fixed=TRUE) # replace dashes with their encoding
 }
 
 for (i in seq_along(MessageLetters)) { #Then convert the message into the, already encoded, morse
   MessageEncoded[[i]] <- mapvalues(MessageLetters[[i]],MorseCode$Letter, MorseCode$Encoding, warn_missing = FALSE)
 }
 
-# concatenate and clean up the excess seperation characters that paste added
-EncodedMessage = sapply(lapply(MessageEncoded, function(x) str_join(x, collapse="|")), function(x) gsub("|/|","/",gsub("||","|",x, fixed=TRUE),fixed=TRUE)) 
+# concatenate 
+MessageEncoded <- lapply(MessageEncoded, function(x) str_c(x, collapse="|"))
+# and clean up the excess seperation characters that paste added
+MessageEncoded <- sapply(MessageEncoded, function(x) gsub("|/|","/",x, fixed=TRUE)) 
 
-writeLines(EncodedMessage, "~/RScripts/CodeOff/code_off-10/code_off-10.out")
+#And write out the result.
+writeLines(MessageEncoded, "~/RScripts/CodeOff/code_off-10/code_off-10.out")
